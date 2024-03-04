a883d19b0831ec2a60bed3c4eb501a7159179cae
Martin Aleksandrov <martin_20.05@abv.bg>
2024-03-04 19:17:25 +0200

    Adding TrainingProgram Entity

--- Pumping_Iron.Core/Data/Models/TrainingProgram.cs
+++ Pumping_Iron.Core/Data/Models/TrainingProgram.cs
@@ -1,6 +1,33 @@
 ﻿namespace Pumping_Iron.Core.Data.Models
 {
+    using static Pumping_Iron.Common.EntityValidationsConstants.ProgramConstants;
+    using System.ComponentModel.DataAnnotations;
     public class TrainingProgram
     {
+        public TrainingProgram()
+        {
+            Clients = new List<Client>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        
+        [Required]
+        [MaxLength(ProgramNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+
+
+        [Required]
+        [MaxLength(MaxDuration)]
+        public int Duration { get; set; }
+
+
+        public ICollection<Client> Clients { get; set; }
     }
 }
