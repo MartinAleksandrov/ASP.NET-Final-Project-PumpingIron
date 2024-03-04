59b3e10e679189e4f33233041d641652aad2582c
Martin Aleksandrov <martin_20.05@abv.bg>
2024-03-04 19:15:47 +0200

    Adding Client Entity

--- Pumping_Iron.Core/Data/Models/Client.cs
+++ Pumping_Iron.Core/Data/Models/Client.cs
@@ -1,6 +1,57 @@
 ﻿namespace Pumping_Iron.Core.Data.Models
 {
+    using static Pumping_Iron.Common.EntityValidationsConstants.ClientConstants;
+    using System.ComponentModel.DataAnnotations;
+    using Pumping_Iron.Core.Data.Models.Enums;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public class Client
     {
+
+        public Client()
+        {
+            ClientId = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid ClientId { get; set; }
+
+
+        [Required]
+        [MaxLength(ClientNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+
+        [Required]
+        [MaxLength(ClientMaxAge)]
+        public int Age { get; set; }
+
+
+        [Required]
+        public Gender Gender { get; set; }
+
+
+        [Required]
+        [ForeignKey(nameof(TrainingProgram))]
+        public int TrainingProgramId { get; set; }
+
+        [Required]
+        public TrainingProgram TrainingProgram { get; set; } = null!;
+
+
+        [Required]
+        [ForeignKey(nameof(Diet))]
+        public int DietId { get; set; }
+
+        [Required]
+        public Diet Diet { get; set; } = null!;
+
+
+        [Required]
+        [ForeignKey(nameof(Trainer))]
+        public int TrainerId { get; set; }
+
+        [Required]
+        public Trainer Trainer { get; set; } = null!;
     }
 }
