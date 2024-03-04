62e522a36194c70634259bc657ea2b56caba0b44
Martin Aleksandrov <martin_20.05@abv.bg>
2024-03-04 19:35:43 +0200

    Adding Diet Entity

--- Pumping_Iron.Core/Data/Models/Diet.cs
+++ Pumping_Iron.Core/Data/Models/Diet.cs
@@ -1,6 +1,27 @@
 ﻿namespace Pumping_Iron.Core.Data.Models
 {
+    using static Pumping_Iron.Common.EntityValidationsConstants.DietConstants;
+    using System.ComponentModel.DataAnnotations;
+
     public class Diet
     {
+        public Diet()
+        {
+            Clients = new List<Client>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(DietNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(DietDescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+
+        
+        public List<Client> Clients { get; set; } 
     }
 }
