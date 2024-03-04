590197afb2bd21d3c85aa002ff97e6d3574059d1
Martin Aleksandrov <martin_20.05@abv.bg>
2024-03-04 19:36:56 +0200

    Adding GymParticipants Entity

---
+++ Pumping_Iron.Core/Data/Models/GymParticipants.cs
@@ -0,0 +1,24 @@
+﻿namespace Pumping_Iron.Core.Data.Models
+{
+    using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+ 
+    public class GymParticipants
+    {
+        [Required]
+        [ForeignKey(nameof(Program))]
+        public int TrainingProgramId { get; set; }
+
+        [Required]
+        public TrainingProgram Program { get; set; } = null!;
+
+
+        [Required]
+        [ForeignKey(nameof(Participant))]
+        public string ParticipantId { get; set; } = string.Empty;
+
+        [Required]
+        public IdentityUser Participant { get; set; } = null!;
+    }
+}
