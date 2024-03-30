c381c77c7bab21effe2d62836e98abfd571062cc
Martin Aleksandrov <martin_20.05@abv.bg>
2024-03-30 12:00:53 +0200

    Creating new Service for Trainig Programs

--- Pumping_Iron.Data/ViewModels/TrainingPrograms/AllTrainingProgramsViewModel.cs
+++ Pumping_Iron.Data/ViewModels/TrainingPrograms/AllTrainingProgramsViewModel.cs
@@ -2,6 +2,14 @@
 {
     public class AllTrainingProgramsViewModel
     {
+        public int Id { get; set; }
 
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public int Duration { get; set; }
+
+        public string ImageUrl { get; set; } = null!;
     }
 }
